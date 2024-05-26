7257a375373c9daff65b6bfa510357cbe8b28894
Rabbi1089 <rabbi.ext@gmail.com>
2024-05-26 22:38:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline bg-green-500 border border-red-700 text-yellow-100">
+      Hello world!
+    </h1>
+    <button className="btn btn-secondary">Secondary</button>
+    <button className="btn btn-info">Info</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import About from "../About/About";
+import Slider from "./Slider";
+
+const Home = () => {
+    return (
+        <div>
+            <Slider />
+            <About />
+        </div>
+    );
+};
+
+export default Home;
